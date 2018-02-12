8dd530e3b3a970b26368df2976e62fff2f2e7252
Chris <essee18@gmail.com>
2018-02-12 21:44:21 +0100

    Tribonacci Sequence codewars

---
+++ TribonacciSequence.java
@@ -0,0 +1,23 @@
+public class Xbonacci {
+
+  public double[] tribonacci(double[] s, int n) {
+    double[] result = initResult(s, n);
+    for (int i = s.length; i < n; i++)
+    {
+      result[i] = result[i - 1] + result[i - 2] + result[i - 3];
+    }
+    return result;
+  }
+
+
+  public double[] initResult(double[] s, int n) {
+    double[] result = new double[n];
+    for (int i = 0; i < s.length && i < n; i ++)
+    {
+      result[i] = s[i];
+    }
+    
+    return result;
+  
+  }
+}
